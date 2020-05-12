83f98ad589f158af4a48aa7420e5a25b05f7d26b
yao-charlie <charlieyao@gmail.com>
2020-05-12 08:24:26 -0700

    2nd commit - experimenting with inheritance

--- classes.py
+++ classes.py
@@ -1,1 +1,33 @@
-print('Hello')
+
+
+
+
+
+class item:
+
+    def __init__(self, name, price, weight):
+        self.name = name
+        self.price = price
+        self.weight = weight
+
+
+class clothing(item):
+
+    def __init__(self, name, price, weight, material, type, temp_high, temp_low):
+        item.__init__(self, name, price, weight)
+        self.type = type
+        self.temp_high = temp_high
+        self.temp_low = temp_low
+        self.material = material
+
+'''
+class consumable(item):
+    def __init__(self, type):
+        self.type = type
+
+
+class potion(consumable):
+
+    def __init__(self, ):
+
+'''
